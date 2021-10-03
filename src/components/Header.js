5defb26491483c74def04f5268a574d6b64c8e28
Lucas Ramos <lucas.ramos@gft.com>
2021-10-03 15:33:16 -0300

    feat(i18n): localized strings on Header and WorkExperience

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-export default function Header({ data }) {
+export default function Header({ data, locale }) {
   return (
     <>
       <h1>Lucas Nogueira Ramos</h1>
--- src/components/WorkExperience.js
+++ src/components/WorkExperience.js
@@ -1,16 +1,24 @@
 import * as React from "react";
 import { renderRichText } from "gatsby-source-contentful/rich-text";
 
-export default function WorkExperience({ data }) {
+export default function WorkExperience({ data, locale }) {
+  const currentJobStringMessage = () => {
+    if (locale === "pt-BR") {
+      return "Cargo Atual";
+    } else {
+      return "Current";
+    }
+  };
   return (
     <>
       {data.edges.map(({ node }) => (
-        <div key={node.id}>
+        <div key={node.id} style={{ marginTop: "40px" }}>
           <h3>
             {node.jobTitle} - {node.company}
           </h3>
           <p>
-            {node.startDate} - {!!node.endDate ? node.endDate : "Current"}
+            {node.startDate} -{" "}
+            {!!node.endDate ? node.endDate : currentJobStringMessage()}
           </p>
           {renderRichText(node.jobDescription)}
         </div>
