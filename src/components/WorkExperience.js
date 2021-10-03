ee955a1f71f2b379c79d9848633a07d169574c39
Lucas Ramos <lucas.ramos@gft.com>
2021-10-03 15:07:34 -0300

    feat: updated components for i18n content

--- src/components/WorkExperience.js
+++ src/components/WorkExperience.js
@@ -1,35 +1,10 @@
 import * as React from "react";
-import { StaticQuery, graphql } from "gatsby";
 import { renderRichText } from "gatsby-source-contentful/rich-text";
 
-export default function WorkExperience() {
-  return <StaticQuery query={query} render={(data) => <Job data={data} />} />;
-}
-
-const query = graphql`
-  query GetAllWorkExperience {
-    allContentfulWorkExperience(filter: { node_locale: { eq: "en-US" } }) {
-      edges {
-        node {
-          id
-          company
-          startDate
-          endDate
-          jobDescription {
-            raw
-          }
-          jobTitle
-        }
-      }
-    }
-  }
-`;
-
-function Job({ data }) {
-  console.log(query, data);
+export default function WorkExperience({ data }) {
   return (
     <>
-      {data.allContentfulWorkExperience.edges.map(({ node }) => (
+      {data.edges.map(({ node }) => (
         <div key={node.id}>
           <h3>
             {node.jobTitle} - {node.company}
